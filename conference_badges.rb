eb9bcbab5e7433178417c47af95bb7a602ccb3b6
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-13 21:43:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,9 +3,7 @@
 speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(speakers)
-  speakers.each do |name|
-    return "Hello, my name is #{name}."
-  end
+  
 end
 
   
