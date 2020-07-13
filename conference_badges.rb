d72fba5b2b3ede6ea9efaf24ab57d269a54cefa9
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-13 21:45:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,13 +1,14 @@
 # Write your code here.
 
-speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+#speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
   
-# def batch_badge_creator(speakers)
-#   speakers.collect{\
+def batch_badge_creator(attendees)
+  attendees.collect{|name| "Hello, my name is #{name}."}
+end
   
   
