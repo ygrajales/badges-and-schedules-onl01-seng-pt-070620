9e17c96c1000cdccd573a556de44b9d4cf201370
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-13 21:47:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,4 +11,6 @@ def batch_badge_creator(attendees)
   attendees.collect{|name| "Hello, my name is #{name}."}
 end
   
-  
+ 
+def assign_rooms(attendees)
+  attendees.collect{|name| "Hello, #{name"
